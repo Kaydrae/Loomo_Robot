bd12fb2cc26e683aa2247a7356377336f96ea539
Brandon Michelsen <brandongmichelsen@gmail.com>
2019-11-13 16:52:00 -0700

    added new elements to the vision class (bind state listeners for head and base)

--- app/src/main/java/com/infintro/loomocart/VisionPresenter.java
+++ app/src/main/java/com/infintro/loomocart/VisionPresenter.java
@@ -2,9 +2,12 @@ package com.infintro.loomocart;
 
 import android.view.Surface;
 
+import com.segway.robot.algo.dts.PersonTrackingListener;
+import com.segway.robot.algo.dts.PersonTrackingProfile;
 import com.segway.robot.sdk.base.bind.ServiceBinder;
-import com.segway.robot.sdk.emoji.module.base.Base;
-import com.segway.robot.sdk.emoji.module.head.Head;
+import com.segway.robot.sdk.locomotion.head.Angle;
+import com.segway.robot.sdk.locomotion.sbv.Base;
+import com.segway.robot.sdk.locomotion.head.Head;
 import com.segway.robot.sdk.vision.DTS;
 import com.segway.robot.sdk.vision.Vision;
 import com.segway.robot.support.control.HeadPIDController;
@@ -22,22 +25,34 @@ public class VisionPresenter {
     private Base mBase;
     private HeadPIDController mHeadPID = new HeadPIDController();
 
+    private PersonTrackingProfile mPersonTracking;
+
     private boolean isHeadBind;
     private boolean isBaseBind;
     private boolean isVisionBind;
 
+    private boolean noObstacles;
+
     private DTS mDTS;
 
+    /* Initialize the Vision Presenter */
     public VisionPresenter(ViewChangeInterface _ViewInterface) {
         mViewInterface = _ViewInterface;
     }
 
     public void startPresenter() {
         mVision = Vision.getInstance();
+        mHead = Head.getInstance();
+        mBase = Base.getInstance();
 
         mVision.bindService(LoomoCart.getContext(), mVisionBindStateListener);
+        mHead.bindService(LoomoCart.getContext(), mHeadStateListener);
+        mBase.bindService(LoomoCart.getContext(), mBaseStateListener);
+
+        mPersonTracking = new PersonTrackingProfile(3, 1.0f);
     }
 
+    /* Stop the vision presenter */
     public void stopPresenter() {
         if (mDTS != null) {
             mDTS.stop();
@@ -47,10 +62,19 @@ public class VisionPresenter {
         mVision.unbindService();
     }
 
+    /* Helper functions */
     public boolean isServicesAvailable() {
-        return isVisionBind;
+
+        return isVisionBind && isBaseBind && isHeadBind;
     }
 
+    private void resetHead() {
+        mHead.setMode(Head.MODE_SMOOTH_TACKING);
+        mHead.setWorldYaw(0);
+        mHead.setWorldPitch(0.7f);
+    }
+
+    /* Service Bind State Listeners */
     private ServiceBinder.BindStateListener mVisionBindStateListener = new ServiceBinder.BindStateListener() {
         @Override
         public void onBind() {
@@ -61,12 +85,69 @@ public class VisionPresenter {
             Surface surface = new Surface(autoFitDrawableView.getPreview().getSurfaceTexture());
             mDTS.setPreviewDisplay(surface);
             mDTS.start();
-            //checkAvailable();
         }
 
         @Override
         public void onUnbind(String reason) {
             isVisionBind = false;
+        }
+    };
+
+    private ServiceBinder.BindStateListener mBaseStateListener = new ServiceBinder.BindStateListener() {
+        @Override
+        public void onBind() {
+            isBaseBind = true;
+        }
+
+        @Override
+        public void onUnbind(String reason) {
+            isBaseBind = false;
+        }
+    };
+
+    private ServiceBinder.BindStateListener mHeadStateListener = new ServiceBinder.BindStateListener() {
+        @Override
+        public void onBind() {
+            isHeadBind = true;
+            resetHead();
+            mHeadPID.init(HeadControlHandlerImpl);
+            mHeadPID.setHeadFollowFactor(1.0f);
+        }
+
+        @Override
+        public void onUnbind(String reason) {
+            isHeadBind = false;
+        }
+    };
+
+    /* Head PID Controller Handler */
+    private HeadPIDController.HeadControlHandler HeadControlHandlerImpl = new HeadPIDController.HeadControlHandler() {
+        @Override
+        public float getJointYaw() {
+            Angle angle = mHead.getHeadJointYaw();
+            if (angle == null) {
+                return 0;
+            }
+            return angle.getAngle();
+        }
+
+        @Override
+        public float getJointPitch() {
+            Angle angle = mHead.getHeadJointPitch();
+            if (angle == null) {
+                return 0;
+            }
+            return angle.getAngle();
+        }
+
+        @Override
+        public void setYawAngularVelocity(float velocity) {
+            mHead.setYawAngularVelocity(velocity);
+        }
+
+        @Override
+        public void setPitchAngularVelocity(float velocity) {
+            mHead.setPitchAngularVelocity(velocity);
         }
     };
 }
